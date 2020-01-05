eaec36e6691191a15d2c06fd33758e4ea507ea85
matt-rule <matt.rule.github@gmail.com>
2020-01-05 20:48:01 +0000

    Add Entity class

---
+++ Entity.cs
@@ -0,0 +1,29 @@
+using OpenTK;
+using OpenTK.Graphics.OpenGL4;
+
+using netcore3_simple_game_engine;
+
+namespace netcore3_simple_game_engine
+{
+    public class Entity
+    {
+        public string Name;
+        public Matrix4 ModelMatrix;
+        public Vertex3d[] Vertices;
+        public uint[] Indices;
+        public int VertexArrayObjectId;
+        public int VertexBufferObjectId;
+        public int IndexBufferObjectId;
+        
+        public Entity(string name, Matrix4 modelMatrix, BufferData bufferData)
+        {
+            Name = name;
+            ModelMatrix = modelMatrix;
+            Vertices = bufferData.Vertices;
+            Indices = bufferData.Indices;
+            VertexArrayObjectId = GL.GenVertexArray();
+            VertexBufferObjectId = GL.GenBuffer();
+            IndexBufferObjectId = GL.GenBuffer();
+        }
+    }
+}
