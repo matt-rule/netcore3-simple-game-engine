2c4c97519718af13a5b7fb3f90166a238047aaf5
matt-rule <matt.rule.github@gmail.com>
2020-01-11 11:44:49 +0000

    Reintroduce "Begin to add texture data"

---
+++ IVertexData.cs
@@ -0,0 +1,13 @@
+using OpenTK;
+using OpenTK.Graphics;
+
+namespace netcore3_opentk_gl_experimental
+{
+    public interface IVertexData
+    {
+        void Render()
+        {
+
+        }
+    }
+}
