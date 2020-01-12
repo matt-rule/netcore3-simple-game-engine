f526bddac4b93e74c7b6d8e52c375f746e92f394
matt-rule <matt.rule.github@gmail.com>
2020-01-12 14:09:41 +0000

    Try to get circle vs circle collisions working (WIP)

--- Collisions.cs
+++ Collisions.cs
@@ -43,7 +43,7 @@ namespace netcore3_simple_game_engine
                 // Rotate the new velocity back.
                 Vector3d postCollisionCircleVelocity = Vector3d.Transform(postCollisionRotatedCircleVelocity, rotationMatrix.Inverted());
 
-                var newVelocity = postCollisionCircleVelocity.Xy * 1.005;
+                var newVelocity = postCollisionCircleVelocity.Xy;
 
                 // Return as a positive collision result.
                 return new CollisionResult {
@@ -62,6 +62,87 @@ namespace netcore3_simple_game_engine
                 };
             }
         }
+        // public static (CollisionResult, CollisionResult) CircleVsCircle(
+        //     double circleRadius1,
+        //     double circleRadius2,
+        //     Vector2d circlePosition1,
+        //     Vector2d circlePosition2,
+        //     Vector2d circleVelocity1,
+        //     Vector2d circleVelocity2
+        // )
+        // {
+        //     Vector2d circle2RelativePosition = circlePosition2 - circlePosition1;
+        //     double distanceBetweenCentres = circle2RelativePosition.Length;
+        //     if (distanceBetweenCentres == 0)
+        //     {
+        //         return (
+        //             new CollisionResult {
+        //                 DidCollide = false,
+        //                 NewCirclePosition = circlePosition1 - new Vector2d(circleRadius1, 0),
+        //                 NewCircleVelocity = circleVelocity1
+        //             },
+        //             new CollisionResult {
+        //                 DidCollide = false,
+        //                 NewCirclePosition = circlePosition2 + new Vector2d(circleRadius1, 0),
+        //                 NewCircleVelocity = circleVelocity2
+        //             }
+        //         );
+        //     }
+
+        //     bool colliding = distanceBetweenCentres < circleRadius1 + circleRadius2;
+        //     if (!colliding)
+        //     {
+        //         // Return original velocity
+        //         return (
+        //             new CollisionResult {
+        //                 DidCollide = false,
+        //                 NewCirclePosition = circlePosition1,
+        //                 NewCircleVelocity = circleVelocity1
+        //             },
+        //             new CollisionResult {
+        //                 DidCollide = false,
+        //                 NewCirclePosition = circlePosition2,
+        //                 NewCircleVelocity = circleVelocity2
+        //             }
+        //         );
+        //     }
+
+        //     Vector2d collisionPoint = circlePosition1 + (circle2RelativePosition.Normalized()) * circleRadius1;
+
+
+        //     double relativeAngleOfCircle2Radians = Math.Atan2(circle2RelativePosition.Y, circle2RelativePosition.X) - Math.PI/2;
+        //     var rotationMatrix = Matrix4d.CreateRotationZ(relativeAngleOfCircle2Radians).Inverted();
+
+        //     Vector3d rotatedCircle1Velocity = Vector3d.Transform(new Vector3d(circleVelocity1.X, circleVelocity1.Y, 0.0), rotationMatrix);
+        //     Vector3d rotatedCircle2Position = Vector3d.Transform(new Vector3d(circle2RelativePosition.X, circle2RelativePosition.Y, 0.0), rotationMatrix);
+        //     Vector3d rotatedCircle2Velocity = Vector3d.Transform(new Vector3d(circleVelocity2.X, circleVelocity2.Y, 0.0), rotationMatrix);
+
+        //     // Make the first circle's Y velocity negative, and the second circle's Y velocity positive.
+        //     Vector3d postCollisionRotatedCircleVelocity1 = new Vector3d(rotatedCircle1Velocity.X, Math.Abs(rotatedCircle1Velocity.Y), 0);
+        //     Vector3d postCollisionRotatedCircleVelocity2 = new Vector3d(rotatedCircle2Velocity.X, -Math.Abs(rotatedCircle2Velocity.Y), 0);
+
+        //     // Rotate the new velocities back.
+        //     Vector3d postCollisionCircleVelocity1 = Vector3d.Transform(postCollisionRotatedCircleVelocity1, rotationMatrix.Inverted());
+        //     Vector3d postCollisionCircleVelocity2 = Vector3d.Transform(postCollisionRotatedCircleVelocity2, rotationMatrix.Inverted());
+
+        //     var newVelocity1 = postCollisionCircleVelocity1.Xy;
+        //     var newVelocity2 = postCollisionCircleVelocity2.Xy;
+
+        //     // Return positive collision result with updated velocities.
+        //     return (
+        //         new CollisionResult {
+        //             DidCollide = true,
+        //             NewCirclePosition = circlePosition1 - circle2RelativePosition.Normalized() * (circleRadius1 + circleRadius2),
+        //             NewCircleVelocity = newVelocity1
+        //         },
+        //         new CollisionResult {
+        //             DidCollide = true,
+        //             NewCirclePosition = circlePosition2 + circle2RelativePosition.Normalized() * (circleRadius1 + circleRadius2),
+        //             NewCircleVelocity = newVelocity2
+        //         }
+        //     );
+        // }
+
         public static CollisionResult CircleVsUnmovableLineSegment(
             // lineV1 and lineV2 are specified clockwise.
             Vector2d lineV1,
@@ -111,7 +192,7 @@ namespace netcore3_simple_game_engine
                 // Rotate the new velocity back.
                 Vector3d postCollisionCircleVelocity = Vector3d.Transform(postCollisionRotatedCircleVelocity, rotationMatrix.Inverted());
 
-                var newVelocity = postCollisionCircleVelocity.Xy * 1.005;
+                var newVelocity = postCollisionCircleVelocity.Xy;
 
                 // Return as a positive collision result.
                 return new CollisionResult {
--- Entity.cs
+++ Entity.cs
@@ -9,12 +9,14 @@ namespace netcore3_simple_game_engine
         public string Name;
         public Matrix4 ModelMatrix;
         public IRenderData RenderData;
+        public Vector2d? Velocity;
         
-        public Entity(string name, Matrix4 modelMatrix, IRenderData renderData)
+        public Entity(string name, Matrix4 modelMatrix, IRenderData renderData, Vector2d? velocity)
         {
             Name = name;
             ModelMatrix = modelMatrix;
             RenderData = renderData;
+            Velocity = velocity;
         }
     }
 }
